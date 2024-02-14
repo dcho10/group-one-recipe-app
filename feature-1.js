cafc9fcc6c32050beeb54d01f76cf13ab23edcc2
Rubal <rubal103@gmail.com>
2024-02-13 19:11:54 -0500

    added images, links and buttons to make content availbale to the user

--- feature-1.js
+++ feature-1.js
@@ -3,7 +3,7 @@ $(function(){
   var searchIngrediant = $("#submit") 
  
   function searchRecipe(event){
-    event.preventDefault()
+  event.preventDefault()
       
     var ingrediantInput= [$("#search").val()];
     
@@ -15,23 +15,48 @@ $(function(){
           })
 
           .then(function (data) {
+            console.log(data)
             for(i=0; i<data.hits.length; i++){
+
             const recipeImg = [data.hits[i].recipe.image]
+            const recipePage = [data.hits[i].recipe.shareAs]
+            const recipeLabel = [data.hits[i].recipe.label]
 
             const containerImg = document.getElementById('image-container');
 
             for (let i = 0; i < recipeImg.length; i++) {
+
+            const recipeRefs = document.createElement('a');  
             const recipeImages= document.createElement('img')
             
             recipeImages.src = recipeImg[i];
-
-            
+            recipeRefs.href= recipePage[i];
+            recipeRefs.role= "button";
+            recipeRefs.textContent= recipeLabel[i];
+          
+           
             containerImg.append(recipeImages)
+            containerImg.append(recipeRefs)
+            
+            
+            $("#image-container").append(containerImg)
+
+            
+            
+              
+              
+
+
+
+
+
+
             
             }
 
-            }
+          }
 
+            
           });
 
   $("#search").val('')

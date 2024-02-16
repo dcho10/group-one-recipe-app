da9f73a7b63a16a089e7efe1e046813d24f06d1b
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-02-16 18:42:17 -0500

    Added ability to see data in console and text adjustments to HTML

--- feature-3.js
+++ feature-3.js
@@ -5,6 +5,11 @@ var searchButton = document.querySelector(".search-button");
 function getApi () {
     var recipeInput = document.getElementById("recipe-input").value;
 
+    if(!recipeInput) {
+        console.error("Enter a recipe");
+        return;
+    }
+
     var requestUrl = "https://api.edamam.com/search?q=" + recipeInput + "&app_id=6c6b865c&app_key=8912fc1bfbb206e14925f8ef4a300a7c";
 
     fetch(requestUrl)
@@ -14,6 +19,15 @@ function getApi () {
     .then (function(data) {
         console.log(data);
 
+        if (data.length === 0) {
+            console.error("Error fetching recipe data:");
+            return;
+        }
+            var hits = data.hits[i].recipe.label;
+            console.log("Hits:", hits);
+
+            var shareAs = data.hits[i].recipe.shareAs;
+            console.log("Share As:", shareAs);
     });
 }
 

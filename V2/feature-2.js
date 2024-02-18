9d5a11462c01c75f7ad28584eb68a55c3c8fbe09
James Hughes <jameshughes2009@hotmail.com>
2024-02-18 09:26:36 -0500

    more edits to java file

--- V2/feature-2.js
+++ V2/feature-2.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    let apiKey = "AIzaSyBaRXW1cQLLhAiCwGrPFD8rB3ia6EUxpSc"
+    var apiKey = "AIzaSyBaRXW1cQLLhAiCwGrPFD8rB3ia6EUxpSc"
 
-    $("form").submit((e) => {
-        e.preventDefault()
-        let search = $("#search").val()
-        videoSearch(apiKey,search,5)//where total videos displayed is set
+    $("form").submit((any) => {
+        any.preventDefault()
+        var search = $("#search").val()
+        videoSearch(apiKey,search,8)//where total videos displayed is set
     })
 })
 
@@ -12,11 +12,11 @@ function videoSearch(apiKey,search,maxResults){
     $.get("https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,(data) => {
         console.log(data)
 
-        let video = ''
+        var video = ''
 
         data.items.forEach(item => {
             video = `
-            <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+            <iframe width="320" height="215" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
             ` // where i can change the video box template
 
             $("#videos").append(video)
